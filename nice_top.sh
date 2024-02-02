911079c0166503f7333748cf1bf78d557b294b96
David Parker <davidparker@flywheel.io>
2024-02-02 13:56:15 -0500

    NSKIP used for both

--- nice_top.sh
+++ nice_top.sh
@@ -6,24 +6,27 @@
 # Or replace Python with whatever command you want to run
 PID=$1
 FIRST=true
-NSKIP=12
 OUTPUT_FILE=top_output2.txt
 SYS=MAC
 if [ $SYS = "MAC" ]; then
   TOPCMD=(top -pid $PID -l 1 -stats 'pid,command,cpu,mem,state')
   TAILCMD=(tail -n +$NSKIP)
   AWKCMD=(awk '{print $0}')
-  SEDCMD=(sed "s/$/ $TIMESTAMP/")
+  NSKIP=12
+
 elif [ $SYS = "LINUX" ]; then
   TOPCMD=(top -p $PID -b -n 1)
-  TAILCMD=(tail -n +8 )
+  TAILCMD=(tail -n +$NSKIP )
   AWKCMD=(awk '{print $1,$12,$9,$10}')
-  SEDCMD=(sed "s/$/ $TIMESTAMP/")
+  NSKIP=7
+
+
 fi
 
 while true
 do
     TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+    SEDCMD=(sed "s/$/ $TIMESTAMP/")
     echo "${TOPCMD[@]}|${TAILCMD[@]}|${AWKCMD[@]}|${SEDCMD[@]} >> $OUTPUT_FILE"
     "${TOPCMD[@]}"|"${TAILCMD[@]}"|"${AWKCMD[@]}"|"${SEDCMD[@]}" >> $OUTPUT_FILE
     if [ "$FIRST" = true ]; then
